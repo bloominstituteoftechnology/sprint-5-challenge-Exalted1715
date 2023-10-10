4afe1fccd1e19d6bdf22bb932b1367c9263ac483
Mitch Hammond <mitch.b.hammond@gmail.com>
2023-10-10 12:50:24 -0600

    work in progress

--- frontend/index.js
+++ frontend/index.js
@@ -5,6 +5,43 @@ async function sprintChallenge5() { // Note the async keyword, in case you wish 
   const currentYear = new Date().getFullYear()
   footer.textContent = `© BLOOM INSTITUTE OF TECHNOLOGY ${currentYear}`
 
+
+
+
+function createLearnerCards(fullName, email, mentors){
+
+
+
+//creating the containers & items
+let container = document.createElement('div')
+let fullName = document.createElement('h3')
+let emailAddress = document.createElement('div')
+let mentorToggle = document.createElement('h4')
+let mentorTitle = document.createElement('ul')
+let listofMentors = docuement.createElement('li')
+
+//adding classlists & Toggles
+container.classList.add('.card')
+mentorToggle.classList.add('h4')
+
+//adding text
+fullName.textContent = `${data1.fullName}`
+emailAddress.textContent = `${data1.email}`
+mentorTitle.textContent = '▶ Mentors'
+listofMentors.textContent = `${data1.mentors}`
+
+//appending
+document.querySelector('section').append(container)
+docuement.querySelector('div').appendChild(fullName)
+docuement.querySelector('div').appendChild(emailAddress)
+docuement.querySelector('div').appendChild(mentorToggle)
+docuement.querySelector('div').appendChild(mentorTitle)
+docuement.querySelector('div').appendChild(listofMentors)
+
+
+return container
+}
+
   // 👆 WORK WORK ABOVE THIS LINE 👆
 }
 
